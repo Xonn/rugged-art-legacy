25c3ac6609cfa094e34deeb6351911a05357f15b
Bastien SOARES <bsp95@hotmail.fr>
2021-04-23 10:43:29 +0200

    Add routes and Navbar refactoring

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+const routes = [
+  { path: '/', name: 'Accueil', component: Homepage, dark: true },
+  { path: '/portfolio', name: 'Portfolio', component: Portfolio, dark: true },
+  { path: '/about', name: 'À propos', component: About, dark: false },
+  { path: 'https://www.etsy.com/fr/shop/RuggedStore', name: 'Shop', external: true },
+  { path: '/contact', name: 'Contact', component: Contact, dark: false },
+];
+
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-greyDark dark:bg-greyLighter">
       <Router>
-        <Navbar />
+        <Navbar routes={routes} />
         <Switch>
-          <Route path="/portfolio"><Portfolio /></Route>
-          <Route path="/about"><About /></Route>
-          <Route path="/shop"><Shop /></Route>
-          <Route path="/contact"><Contact /></Route>
+          {routes.filter(route => !route.external).map(route => (
+            <Route path={route.path} exact render={() => (<route.component dark={route.dark} />)} />
+          ))}
         </Switch>
       </Router>
     </div>
   );
 }
 
-function Portfolio() {
-  return <h2>Portfolio</h2>;
-}
-
-function About() {
-  return <h2>À propos</h2>;
-}
-
-function Shop() {
-  return <h2>Shop</h2>;
-}
-
-function Contact() {
-  return <h2>Contact</h2>;
-}
 export default App;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,60 +1,59 @@
 import React from 'react'
-import Logo from '../../images/logo.png'
+import logo from '../../images/logo.png'
+import logoDark from '../../images/logoDark.png'
 import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({routes}) => {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
 
     return (
         <>
-            <nav className="relative flex flex-wrap items-center justify-between p-5 mb-3 mx-36">
-                <div className="container flex flex-wrap items-center justify-between px-4 mx-auto">
-                    <div className="relative flex justify-center w-full lg:w-auto lg:static lg:block lg:justify-start">
-                        <a className="inline-block py-2 mr-4 text-sm font-bold leading-relaxed text-white uppercase whitespace-nowrap" href="#pablo">
-                            <img className="w-44" src={Logo} alt="logo"/>
-                        </a>
+            <Nav>
+                <div className="container flex flex-wrap items-center justify-between px-4">
+                    <div className="relative flex justify-between w-full lg:w-auto lg:static lg:block lg:justify-start">
+                        <NavLink to="/" className="inline-block py-2 mr-4 text-sm font-bold leading-relaxed text-white uppercase whitespace-nowrap">
+                            <img className="block w-2/3 md:w-7/12 dark:hidden" src={logo} alt="logo"/>
+                            <img className="hidden w-2/3 md:w-7/12 dark:block" src={logoDark} alt="logo"/>
+                        </NavLink>
                         <button
                             className="block px-3 py-1 text-xl leading-none text-white bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer lg:hidden focus:outline-none"
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}>
-                            <i className="fas fa-bars"></i>
+                            <svg className="w-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
                         </button>
                     </div>
                 
-                    <div className={"lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")}>
+                    <div className={"lg:flex flex-grow justify-center items-center" + (navbarOpen ? " flex" : " hidden")}>
                         <ul className="flex flex-col items-center list-none text-md font-openSans text-grey lg:flex-row lg:ml-auto">
-                            <li className="nav-item">
-                                <NavLink to="/" className="flex items-center m-3 text-xs font-bold leading-snug hover:text-greyLight hover:opacity-75">
-                                    {/* <svg className="w-6 h-6 font-bold text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" /></svg> */}
-                                    <svg className="w-8 h-8 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" /></svg>
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/portfolio" activeClassName="text-greyLight" className="flex items-center m-3 font-bold hover:text-greyLight">
-                                    <span className="ml-2">Portfolio</span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/about" activeClassName="text-greyLight" className="flex items-center m-3 font-bold hover:text-greyLight">
-                                    <span className="ml-2">À propos</span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/shop" activeClassName="text-greyLight" className="flex items-center m-3 font-bold hover:text-greyLight">
-                                    <span className="ml-2">Shop</span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="contact" activeClassName="text-greyLight" className="flex items-center m-3 font-bold hover:text-greyLight">
-                                    <span className="ml-2">Contact</span>
-                                </NavLink>
-                            </li>
+                            <NavLink to='/' className="flex items-center m-3 text-xs font-bold leading-snug hover:text-greyLight hover:opacity-75">
+                                <svg className="w-8 h-8 text-primary dark:text-secondary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
+                                </svg>
+                            </NavLink>
+                            {routes.filter(route => route.path !== '/').map(route => {
+                                return route.external ?
+                                    <li className="nav-item">
+                                        <NavLink to={{ pathname : route.path }} target="_blank" activeClassName="text-greyLight dark:text-greyDarker" className="flex items-center m-3 font-bold hover:text-greyLight dark:hover:text-greyDarker">
+                                            <span className="ml-2">{route.name}</span>
+                                        </NavLink>
+                                    </li>
+                                :
+                                    <li className="nav-item">
+                                        <NavLink to={route.path} activeClassName="text-greyLight dark:text-greyDarker" className="flex items-center m-3 font-bold hover:text-greyLight dark:hover:text-greyDarker">
+                                            <span className="ml-2">{route.name}</span>
+                                        </NavLink>
+                                    </li>
+                            })} 
                         </ul>
                     </div>
                 </div>
-            </nav>
+            </Nav>
         </>
     );
 }
+
+const Nav = ({children}) => (
+    <nav className="relative flex flex-wrap items-center justify-between p-5 mx-auto mb-3 md:w-4/5">{children}</nav>
+);
 
 export default Navbar
