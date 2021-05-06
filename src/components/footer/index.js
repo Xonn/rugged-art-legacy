af48781770bdcd1a80c4e25a0e50a2c3b059a6bf
Bastien SOARES <bsp95@hotmail.fr>
2021-05-06 12:59:17 +0200

    Use socialNetwork.js data in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/footer/logo.png';
-import facebook from '../../images/footer/bt_ft_scroll_facebook_no.svg';
-import behance from '../../images/footer/bt_ft_scroll_behance_no.svg';
-import instagram from '../../images/footer/bt_ft_scroll_instagram_no.svg';
-import linkedin from '../../images/footer/bt_ft_scroll_linkedin_no.svg';
-import twitter from '../../images/footer/bt_ft_scroll_twitter_no.svg';
-import dribbble from '../../images/footer/bt_ft_scroll_dribbble_no.svg';
-import etsy from '../../images/footer/bt_ft_scroll_etsy_no.svg';
-import twitch from '../../images/footer/bt_ft_scroll_twitch_no.svg';
-
-const socialNetwork = [
-    {name: "Facebook", url: "https://www.facebook.com/ArtsbyRugged", image: facebook},
-    {name: "Behance", url: "https://www.facebook.com/ArtsbyRugged", image: behance},
-    {name: "Instagram", url: "https://www.facebook.com/ArtsbyRugged", image: instagram},
-    {name: "LinkedIn", url: "https://www.facebook.com/ArtsbyRugged", image: linkedin},
-    {name: "Twitter", url: "https://www.facebook.com/ArtsbyRugged", image: twitter},
-    {name: "Dribbble", url: "https://www.facebook.com/ArtsbyRugged", image: dribbble},
-    {name: "Etsy", url: "https://www.facebook.com/ArtsbyRugged", image: etsy},
-    {name: "Twitch", url: "https://www.facebook.com/ArtsbyRugged", image: twitch},
-];
+import {socialNetwork} from '../../data/socialNetwork';
 
 const Index = ({routes}) => {
     return (
