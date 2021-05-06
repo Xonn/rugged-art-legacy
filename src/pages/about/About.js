e6a308235aa8a98a8c49f1c0cc691a41c9e3edd9
Bastien SOARES <bsp95@hotmail.fr>
2021-05-06 10:57:23 +0200

    Change About page responsive display

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -21,6 +21,14 @@ const interests = [
     { name: "Jeux Vidéo", image: game, description: "Phasellus nec sapien non nulla imperdiet aliquam et eget libero. Ut lacinia dui massa, eget eleifend enim euismod in. Donec mattis tristique erat, eget gravida risus. Nulla tincidunt est et enim auctor euismod." },
 ];
 
+const experiences = [
+    { image: job, year: "2014 > 2016",
+        period: "2 ans", 
+        establishment: "Lycée des métiers de la chaine graphique Claude-Garamont,", 
+        place: "à Colombe", 
+        degree: "BTS Design graphique option communication et médias imprimés"},
+    { image: school, year: "2011 > 2014", period: "1 an", establishment: "Lycée du livre et des arts graphiques Maximilien-Vox,", place: "à Paris", degree: "Baccalauréat STD2A (Sciences et Technologies du Design et des Arts Appliqués)"}
+]
 const About = ({dark}) => {
     if (dark) {
         document.body.classList.remove('dark');
@@ -30,79 +38,77 @@ const About = ({dark}) => {
     
     const [activeIndex, setActiveIndex] = useState(0);
 
-    console.log(activeIndex);
-
     return (
-        <div className="relative w-5/6 m-auto">
+        <div className="relative w-5/6 px-5 py-10 m-auto">
             <Tabs>
                 <TabPanel className="flex flex-wrap justify-center">
-                    <div className="relative w-1/3">
-                        <img className="w-80" src={portrait} alt=""/>
-                        <img className="absolute w-40 -top-10 -right-16" src={hello} alt=""/>
-                        <img className="absolute w-full -left-10 top-20" src={particle} alt=""/>
+                    <div className="w-full text-center lg:w-1/3">
+                        <div className="relative inline-block text-center">
+                            <img className="w-40 md:w-80" src={portrait} alt=""/>
+                            <img className="absolute w-20 md:w-40 -right-10 -top-8 md:-top-12 md:-right-24" src={hello} alt=""/>
+                            <img className="absolute w-full left-5 top-20" src={particle} alt=""/>
+                        </div>
                     </div>
-                    <div className="flex items-center w-1/2 text-2xl font-openSans">
+                    <div className="flex items-center w-full mb-10 text-xl text-justify md:w-4/5 lg:ml-20 lg:w-1/2 font-openSans">
                         <p>
                             Je m’appelle <b>Dylan Rambinaising, graphiste &amp; illustrateur freelance depuis 2015.</b> Animé par l’amour des arts depuis tout petit j’ai grandi en me nourrissant de tout ce qui m’entourait. Toujours à la recherche de nouveaux défis je me considère comme un créatif compulsif qui vous invites à découvrir son univers sous l’alias de <b>Rugged*.</b>
                         </p>
-                        
                     </div>
                 </TabPanel>
 
                 <TabPanel>
-                <div className="flex flex-wrap items-center justify-center w-full">
-                    <div className="flex justify-center w-1/6 m-5">
-                        <img className="w-52" src={job} alt=""/>
-                    </div>
-                    <div className="flex justify-center w-4/6 text-xl font-openSans">
-                        <div className="flex flex-col w-1/6 text-secondary">
-                            <span className="font-bold">2014 &gt; 2016</span>
-                            <span>2 ans</span>
+                    {experiences.map(experience =>         
+                        <div className="flex flex-wrap items-start justify-center w-full">
+                            <div className="flex justify-center w-1/2 my-5 md:w-1/5">
+                                <img className="w-52" src={experience.image} alt=""/>
+                            </div>
+                            <div className="flex flex-col items-center w-full mb-5 mr-3 md:mt-20 md:items-start md:w-1/12 text-secondary">
+                                <span className="font-bold">{experience.year}</span>
+                                <span>{experience.period}</span>
+                            </div>
+                            <div className="flex flex-col justify-start w-3/5 mb-5 text-xl md:mt-20 md:flex-row font-openSans">
+                                <p className="flex flex-col w-full font-semibold text-center md:w-5/6 md:text-left">
+                                    <div>
+                                        {experience.establishment}
+                                        <span className="font-light text-grey"> {experience.place}</span>
+                                    </div>
+                                    <span className="font-bold font-futura text-grey">{experience.degree}</span>
+                                </p>
+                            </div>
                         </div>
-                        <p className="flex flex-col">Lycée des métiers de la chaine graphique Claude-Garamont, <span className="font-thin">à Colombe</span>
-                            <span className="font-bold font-futura text-grey">BTS Design graphique option communication et médias imprimés</span></p>
-                    </div>
-                </div>
-
-                <div className="flex flex-wrap items-center justify-center">
-                    <div className="flex justify-center w-1/6 m-5">
-                        <img className="w-52" src={school} alt=""/>
-                    </div>
-                    <div className="flex justify-center w-4/6 text-xl font-openSans">
-                        <div className="flex flex-col w-1/6 text-secondary">
-                            <span className="font-bold">2014 &gt; 2016</span>
-                            <span>2 ans</span>
-                        </div>
-                        <p className="flex flex-col">Lycée des métiers de la chaine graphique Claude-Garamont, <span className="font-thin">à Colombe</span>
-                            <span className="font-bold font-futura text-grey">BTS Design graphique option communication et médias imprimés</span></p>
-                    </div>
-                </div>
+                    )}
                 </TabPanel>
 
-                <TabPanel className="relative flex items-center justify-center pt-10 pb-40">
-                    {interests.map((interest, index) =>
-                        <>
-                            <div className={`justify-center w-1/5 ${index === 0 ? `-mt-12`: ``}`}>
-                                <div className="flex items-center justify-center mb-5">
-                                    <div>
-                                        <img onClick={() => {setActiveIndex(index)}} className="w-52" src={interest.image} alt=""/>
-                                        <span className="flex justify-center mt-5 text-lg font-openSans text-greyLight">{interest.name}</span>
-                                        <p className={`absolute w-2/5 text-justify font-futura mt-5 bg-secondary bg-opacit0 p-3 ${ activeIndex === index ? 'visible' : 'hidden'}`} dangerouslySetInnerHTML={{ __html: interest.description}}></p>
+                <TabPanel className="relative">
+                    <div className="flex flex-row flex-wrap items-center justify-center">
+                        {interests.map((interest, index) =>
+                            <>
+                                <div className={`justify-center w-1/3 md:w-1/5 p-2 md:p-8 ${index === 0 ? `-mt-11`: ``}`}>
+                                    <div className="relative flex items-center justify-center mb-5">
+                                        <div>
+                                            <img onClick={() => {setActiveIndex(index)}} className="w-52" src={interest.image} alt=""/>
+                                            <span className="flex justify-center mt-5 text-lg font-openSans text-greyLight">{interest.name}</span>
+                                        </div>
+                                        {(index < (interests.length -1) ? 
+                                            (<span className={`absolute hidden md:inline-block w-5 h-5 m-5 rounded-full -right-14 bg-secondary ${index === 0 ? `top-28`: ``}`}></span>)
+                                            :
+                                            (<div></div>)
+                                        )}
                                     </div>
-                                    {(index < (interests.length -1) ? 
-                                        (<span className="inline-block w-5 h-5 m-5 rounded-full bg-secondary"></span>)
-                                        :
-                                        (<div></div>)
-                                    )}
                                 </div>
-                            </div>
-                        </>
-                    )}
+                            </>
+                        )}
+                    </div>
+                    <div className="h-48">
+                        {interests.map((interest, index) =>
+                            <p className={`w-full lg:w-2/3 justify-center m-auto text-justify font-futura bg-secondary bg-opacit0 p-5 ${ activeIndex === index ? 'visible' : 'hidden'}`} dangerouslySetInnerHTML={{ __html: interest.description}}></p>
+                        )}
+                    </div>
                 </TabPanel>
                 <TabPanel>
                     Compétences
                 </TabPanel>
-                <TabList className="absolute bottom-0 left-0 right-0 flex flex-wrap justify-center text-xl tab font-minion text-greyDarker">
+                <TabList className="absolute bottom-0 left-0 right-0 flex flex-wrap justify-center mt-20 text-xl tab font-minion text-greyDarker">
                     <Tab className="leading-3 border-b-8 border-opacity-50 border-secondary p-0.5">
                         Introduction
                     </Tab>
